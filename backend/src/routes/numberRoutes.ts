85ee05beeb380c51d83a9375da251d3c2a87457c
gondav <viktoria.gonda@gmail.com>
2023-06-22 12:28:24 +0200

    wrap numberController function into errorChecker

--- backend/src/routes/numberRoutes.ts
+++ backend/src/routes/numberRoutes.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import { numberController } from '../controllers/numberController';
+import errorChecker from '../middlewares/errorChecker';
 
 export const numberRouter = express.Router();
 
-numberRouter.get('/number', numberController.getNumber);
-numberRouter.post('/number', numberController.saveNumber);
+numberRouter.get('/number', errorChecker(numberController.getNumber));
+numberRouter.post('/number', errorChecker(numberController.saveNumber));
