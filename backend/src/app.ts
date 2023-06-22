6e3c1ca3c059aa901c225b48ed5bf18a857ca626
gondav <viktoria.gonda@gmail.com>
2023-06-22 11:53:14 +0200

    add app.use(express.json()) to be able to send post requests + add /api to endpoint

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,9 @@
 import express from 'express';
+import { numberRouter } from './routes/numberRoutes';
 
 const app = express();
+app.use(express.json());
+
+app.use('/api', numberRouter);
 
 export default app;
