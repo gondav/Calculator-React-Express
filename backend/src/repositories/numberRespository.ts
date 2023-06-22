f2dab1c59aa450ee2d964994c0af5116334fe95b
gondav <viktoria.gonda@gmail.com>
2023-06-22 10:56:42 +0200

    add numberRepository

---
+++ backend/src/repositories/numberRespository.ts
@@ -0,0 +1,18 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+export const numberRespository = {
+  async getNumber(): Promise<string> {
+    return await fs.readFile(
+      path.join(__dirname, '../data', 'data.txt'),
+      'utf-8'
+    );
+  },
+
+  async saveNumber(number: Number): Promise<void> {
+    await fs.writeFile(
+      path.join(__dirname, '../data', 'data.txt'),
+      number.toString()
+    );
+  },
+};
