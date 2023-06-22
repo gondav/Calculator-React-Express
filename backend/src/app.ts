4c44a61e9ade8a53d20ea72b02197f2a652bb57d
gondav <viktoria.gonda@gmail.com>
2023-06-22 12:15:41 +0200

    add global errorHandler middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import { numberRouter } from './routes/numberRoutes';
+import errorHandler from './middlewares/errorHandler';
 
 const app = express();
 app.use(express.json());
 
 app.use('/api', numberRouter);
+app.use(errorHandler);
 
 export default app;
