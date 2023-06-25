e24941eda16da9d539d243f8d133a3274650b41d
gondav <viktoria.gonda@gmail.com>
2023-06-25 12:07:17 +0200

    set data dynamically in Display component

--- frontend/src/components/Display.tsx
+++ frontend/src/components/Display.tsx
@@ -1,11 +1,21 @@
-const Display = () => {
+import { State } from '../models/state';
+
+interface DisplayProps {
+  state: Readonly<State>;
+}
+
+const Display = ({ state }: DisplayProps) => {
   return (
     <div className="display flex flex-col items-end justify-between  bg-neutral-700 border-b">
       <div className="previous-operand text-neutral-400">
-        <p className="pt-2 px-4">66 +</p>
+        <p className="pt-2 px-4">
+          {state.previousOperand} {state.operation}
+        </p>
       </div>
       <div className="current-operand text-neutral-100 text-3xl">
-        <p className="text-right px-4 pb-4 max-w-xs break-words">12</p>
+        <p className="text-right px-4 pb-4 max-w-xs break-words">
+          {state.currentOperand}
+        </p>
       </div>
     </div>
   );
