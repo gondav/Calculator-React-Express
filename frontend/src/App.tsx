070b75b9d076f388e81f6efd994dd35572a3e342
gondav <viktoria.gonda@gmail.com>
2023-06-25 11:22:56 +0200

    add App.tsx with Display component

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import Display from './components/Display';
+
+const App = () => {
+  return (
+    <div className="main-container">
+      <div className="mx-auto w-80">
+        <Display />
+      </div>
+    </div>
+  );
+};
+
+export default App;
